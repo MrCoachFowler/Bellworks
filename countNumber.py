7fafb89c087d0442baa824df24fe9e58ef9ddaa7
MrCoachFowler <tfowler@tanq.org>
2024-09-30 07:48:24 -0700

    9-30 class update

---
+++ countNumber.py
@@ -0,0 +1,26 @@
+###Function Requirements
+
+#Name: countNumbers
+#Purpose: Print numbers from 1 to the number specified by the input
+#Result: void
+#Input: number to count to (int)
+def countNumbers(numRange):
+    #write the code for our function
+
+
+
+
+
+
+
+
+###Program Requirements
+###Using your function, write a program that:
+
+#TODO: prints the number 1 to 50
+countNumbers(50)
+
+#TODO prints the number 1 to 200
+countNumbers(200)
+
+#CHALLENGE TODO prints 1 to a random number between 20 and 50
