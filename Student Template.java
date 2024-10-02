fa9f206122423a4c9e85a283fc7d1e1810a8f801
kingbernoulli <tybfowler@gmail.com>
2024-10-02 09:52:12 -0700

    test and student templates

---
+++ Student Template.java
@@ -0,0 +1,30 @@
+public class Main {
+    //Make attributes to store the following information:
+    //id
+    //tests
+
+    
+    //make a constructor
+    //updates ids
+
+    //Make a method following the contract
+    //Name: addTest
+    //Purpose: adds a new test to tests
+    //Input: test
+    //Output: void
+
+    //Make a method following the contract
+    //Name: getTestsByType
+    //Purpose: gets an array of tests the student has taken by their type
+    //Input: test type
+    //Output: an array of the tests taken of that subject
+
+    //Make a method following the contract
+    //Name: 
+    //Purpose: 
+    //Input: 
+    //Output:
+
+    //write a test file that:
+    //
+}
---
+++ Test Template.java
@@ -0,0 +1,22 @@
+public class Main {
+    //Make attributes to store the following information:
+    //name
+    //school session
+    //type
+    //answer choices
+    //correct answers
+    //score
+
+    //make a constructor
+    //updates name, type, and school session
+
+    //Make a method following the contract
+    //Name: scoreTest
+    //Purpose: Compares the answers to the key
+    //Input: test key answers
+    //Output: void (updates correct answers and score)
+
+
+    //write a run file that:
+    //
+}
