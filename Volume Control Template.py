28ecaa576a043069b9b50e3ca868d4a644767c38
MrCoachFowler <tfowler@tanq.org>
2024-10-02 20:36:44 -0700

    volumeControl

---
+++ Volume Control Template.py
@@ -0,0 +1,9 @@
+#Make a function based on the following contract:
+#Name: volumeControl
+#Purpose: looks through a list of words and make uppercase if 'loud' or lowercase if 'quiet'
+#Input: list of words
+#Output: list of volume controlled words
+
+#write a program that:
+#takes in the following list and prints out a version where 'loud' is capitalized and 'quit' is lowercase
+#['Medium', 'Mid', 'Lower-mid', 'Quiet', 'Quit', 'Not quiet', 'Loud', 'Screaming']
