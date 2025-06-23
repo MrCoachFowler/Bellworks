5a9f68435760f5a6c8f0ec012d4ab2773ba828f8
MrCoachFowler <tfowler@tanq.org>
2025-06-23 11:39:26 -0700

    manual mod activities

---
+++ AP A/ManualModTemplate.java
@@ -0,0 +1,17 @@
+public class ManualModTemplate {
+    public static void main(String[] args)
+    {
+        // Write a static method to compute the modulus of two numbers without using a modulus function or operator
+        // Then use it to print the result of:
+        //     254 % 12 (2)
+        //     85304 % 17 (15)
+        //     2084729384 % 23843 (16679)
+        
+        // you go here...
+    }
+
+    public static int manMod(int a, int b)
+    {
+        //you go here...
+    }
+}
--- Intro/FindMaxInArray Template.py
+++ Intro/FindMaxInArray Template.py
@@ -24,34 +24,9 @@ def getTestArray():
 #Purpose: Print the row and column index of the brightest cell (largest sum), the most red cell, the most green cell, and the most blue cell
 #Input: m x n array of color lists
 #Output: Void
-def findMaxIndexInColorArray(arr):
-    winnerIndexes = [[-1, -1], [-1, -1], [-1, -1], [-1, -1]]
-    biggestValues = [0,0,0,0]
-    #index 0 is reddest
-    #index 1 is greenest
-    #index 2 is bluest
-    #index 3 is brightest
-    for i in range(len(arr)):
-        row = arr[i]
-        for j in range(len(row)):
-            cell = row[j]
-            cellSum = 0
-            for rgb in range(len(cell)):
-                value = cell[rgb]
-                cellSum += value
-                if value > biggestValues[rgb]:
-                    biggestValues[rgb] = value
-                    winnerIndexes[rgb] = [i, j]
-            if cellSum > biggestValues[3]:
-                biggestValues[3] = cellSum
-                winnerIndexes[3] = [i, j]
-    labels = ["Reddest", "Greenest", "Blueest", "Brightest"]
-    for i in range(len(labels)):
-        print("The" , labels[i], "was row", winnerIndexes[i][0] , "column", winnerIndexes[i][1], "with a value of", biggestValues[i])
+
 
 #write a program that:
 #Creates a test array using the given function
 #uses the findMaxIndexInColorArray function to print the row and column index of the brightest cell, most red cell, most green cell, and most blue cell
 #CHALLENGE: if there's more than one cell that satisfies the requirement, list them all
-testArray = getTestArray()
-findMaxIndexInColorArray(testArray)
